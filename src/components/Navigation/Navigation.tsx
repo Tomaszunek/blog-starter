126d0853ba79651e5672e74d056a49f26f36c46b
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-12-03 12:58:16 +0100

    make better stucture of 2 colors

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,12 +5,35 @@ import Links from './navLinks';
 import './navigation.scss';
 import BurgerMenu from './BurgerMenu';
 
-export default class Navigation extends React.Component {    
+export default class Navigation extends React.Component<IProps, IState> {
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+        navBar: ''
+    };        
+}    
+
+  
+  public componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+}
+
+  public componentWillUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  public handleScroll = () => { 
+      this.setState({ 
+        navBar: (window.pageYOffset > 80 ? 'navigation white_background' : ''), 
+      });
+  }
   public render() {
     return (
-      <nav className="navigation">
+      <nav className={this.state.navBar}>
         <div className="logo">
-          <NavLink to="/">ProHuman</NavLink>
+          <NavLink to="/">prohuman</NavLink>
         </div>
         <Links/>
         <div className="login">
@@ -20,4 +43,11 @@ export default class Navigation extends React.Component {
       </nav>
     );
   }
+}
+
+interface IProps {
+  someVrbl?: any
+}
+interface IState {
+  navBar: string
 }
