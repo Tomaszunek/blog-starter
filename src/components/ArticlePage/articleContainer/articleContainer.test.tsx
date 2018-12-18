f4393a73006bb922b83198ff1e2f684fcad095c6
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-12-18 21:24:32 +0100

    test for articleConteiner

--- src/components/ArticlePage/articleContainer/articleContainer.test.tsx
+++ src/components/ArticlePage/articleContainer/articleContainer.test.tsx
@@ -1,15 +1,49 @@
-// import * as React from 'react';
-// import { mount } from 'enzyme';
-// import ArticlePageComp from './ArticlePageComp'
+import * as React from 'react';
+import { mount } from 'enzyme';
+import * as renderer from 'react-test-renderer';
+import ArticleContainer from './articleContainer'
+import { IArticleModel } from 'src/models';
 
-describe('ArticleContainer', () => {
-  describe('when isLoading is false', () => {
-    it('should render children', () => {      
-      // const wrapper = mount(
-      //   <div>abc</div>
-      // );
-      // expect(wrapper.html()).toEqual('<div class=\"loadingScreen\">LoadingScreen</div>');
-      // wrapper.unmount();
-    });
+describe('Error Page', () => {
+  describe('Component should render right', () => {
+    let app:any;    
+    const articles:Array<IArticleModel> = [{
+      id: 1,
+      type: "ability",
+      articleType: "ability",
+      body: "body",
+      createdAt: new Date(),
+      image: "images/placeholder/abc.jpg",
+      name: "woda",
+      slug: "woda",
+      updatedAt: new Date(),
+    },{
+      id: 2,
+      type: "ability",
+      articleType: "ability",
+      body: "body",
+      createdAt: new Date(),
+      image: "images/placeholder/abc.jpg",
+      name: "woda",
+      slug: "woda",
+      updatedAt: new Date(),
+    }]
+    beforeEach(() => {
+      app = mount(<ArticleContainer  articles={articles} structure={[4,4]}/>);
+    })
+    it('render correctly snapshot component', () => {
+      const tree = renderer
+      .create(<ArticleContainer articles={articles} structure={[4,4]}/>)
+      .toJSON();
+      expect(tree).toMatchSnapshot();
+    })
+    it('expect two rendered tile', () => {
+      expect(app.find('.articleTile').length).toEqual(2);
+    })
+
+    it('expect component elements', () => {
+      expect(app.find('.articleConteiner').length).toEqual(1);
+      expect(app.find('.grid.grid-four').length).toEqual(2);
+    })
   });
 });
