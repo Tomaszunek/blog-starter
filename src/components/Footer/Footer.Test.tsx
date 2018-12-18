ba19f7fbbde6e9a0a7d93627c7f5770873b54b14
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-12-18 23:38:32 +0100

    make footer tests tdd

--- src/components/Footer/Footer.Test.tsx
+++ src/components/Footer/Footer.Test.tsx
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 import * as renderer from 'react-test-renderer';
 import Footer from './Footer'
 
-describe('Error Page', () => {
+describe('Footer Page', () => {
   describe('Component should render right', () => {
     let app:any;             
     beforeEach(() => {
@@ -15,8 +15,26 @@ describe('Error Page', () => {
       .toJSON();
       expect(tree).toMatchSnapshot();
     })
-    it.skip('should render proper count item', () => {
-      console.log(app.debug())
+    it('should contain links', () => {
+      expect(app.find('a').length).toEqual(12);
+    })
+    it('should contain links containar', () => {
+      expect(app.find('.links').length).toEqual(1);      
+    })
+    it('should contain links', () => {
+      expect(app.find('.facebook').length).toEqual(1);
+      expect(app.find('.linkedin').length).toEqual(1);
+      expect(app.find('.patronite').length).toEqual(1);
+      expect(app.find('.twitter').length).toEqual(1);
+      expect(app.find('.twitch').length).toEqual(1);
+      expect(app.find('.youtube').length).toEqual(1);
+    })
+    it('should contain copyright', () => {      
+      expect(app.contains(<span>&copy; Created in 2018 by Tomasz Pawłowski. All rights reserved.</span>)).toBeTruthy();
+    })
+    it('should contain contact form', () => {      
+      expect(app.find('form').length).toEqual(1);
+      expect(app.find('button[type="submit"]').length).toEqual(1);
     })
   });
 }); 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,9 +5,64 @@ export default class Footer extends React.Component {
   public render() {
     return (
       <footer className="footer">
-        footer
+        <div className="upperFooter">
+          <div className="links">
+            {this.displayLinksBasedOnEnv(1)}          
+            <a className="linkedin" href="">LinkedIn</a>
+            <a className="patronite" href="">Patronite</a>
+            <a className="twitter" href="">Twitter</a>
+            {this.displayLinksBasedOnEnv(2)}
+            <a className="facebook" href="">Facebook</a>
+            <a className="twitch" href="">Twitch</a>
+            <a className="youtube" href="">Youtube</a>
+          </div>
+          <form action="">
+            <input type="text"/>
+            <textarea name="" id="" cols={30} rows={10} defaultValue=""/>
+            <button type="submit">Send</button>
+          </form>          
+        </div>
+        <span>&copy; Created in 2018 by Tomasz Pawłowski. All rights reserved.</span>
       </footer>
     );
+  }
+
+  private displayLinksBasedOnEnv = (num:number) => {
+    if(process.env.serv_type === "blog") {
+      if(num === 1) {
+        return <>
+            <a href="articles/tech">technology</a>
+            <a href="articles/spiritual">spiritual</a>
+            <a href="articles/psychology">psychology</a>
+        </>
+      } else {
+        return <>
+            <a href="store">store</a>
+            <a href="articles/motivation">motivation</a>
+            <a href="login">login</a>   
+        </>
+      }
+    } else if(process.env.serv_type === "games") {
+      if(num === 1) {
+        return <>
+            <a href="articles/news">news</a>
+            <a href="projects">projects</a>
+            <a href="abilities">abilities</a>   
+        </>
+      } else {
+        return <>
+            <a href="store">store</a>
+            <a href="about">about</a>
+            <a href="login">login</a>   
+        </>
+      }
+    } else  {
+      return <>
+          <a href="">abc</a>
+          <a href="">abc</a>
+          <a href="">abc</a>   
+      </>
+    }
   }
 }
 
