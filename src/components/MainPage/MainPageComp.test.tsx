0b5a09a56ef795a97b40b3814b772af0679c1b9b
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-12-23 10:12:07 +0100

    test for main page cont

--- src/components/MainPage/MainPageComp.test.tsx
+++ src/components/MainPage/MainPageComp.test.tsx
@@ -1,15 +1,28 @@
-// import * as React from 'react';
-// import { mount } from 'enzyme';
-// import ArticlePageComp from './ArticlePageComp'
+import * as React from 'react';
+import { mount } from 'enzyme';
+import * as renderer from 'react-test-renderer';
+import MainPageComp from './MainPageComp'
 
-describe('Main Page Component', () => {
-  describe('when isLoading is false', () => {
-    it('should render children', () => {      
-      // const wrapper = mount(
-      //   <div>abc</div>
-      // );
-      // expect(wrapper.html()).toEqual('<div class=\"loadingScreen\">LoadingScreen</div>');
-      // wrapper.unmount();
-    });
+describe('Footer Page', () => {
+  describe('Component should render right', () => {
+    let app:any;             
+    beforeEach(() => {
+      app = mount(<MainPageComp/>);
+    })
+    it('render correctly snapshot component', () => {
+      const tree = renderer
+      .create(<MainPageComp/>)
+      .toJSON();
+      expect(tree).toMatchSnapshot();
+    })    
+    it('should contain main containar', () => {
+      expect(app.find('main').length).toEqual(1);      
+    })
+    it('should contain elements', () => {
+      expect(app.find('.fullScreenCont').length).toEqual(2);
+      expect(app.find('.sliderPhoto').length).toEqual(1);
+      expect(app.find('.articleConteiner').length).toEqual(1);
+      expect(app.find('.grid.grid-four').length).toEqual(2);      
+    })
   });
-});
+}); 
