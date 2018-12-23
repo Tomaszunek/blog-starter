91a3324b1d2623339101a99666b406598715baf3
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-12-23 10:57:17 +0100

    add snapshot

--- src/components/ProductSinglePage/productSinglePageComp.test.tsx
+++ src/components/ProductSinglePage/productSinglePageComp.test.tsx
@@ -1,15 +1,45 @@
-// import * as React from 'react';
-// import { mount } from 'enzyme';
-// import ArticlePageComp from './ArticlePageComp'
+import * as React from 'react';
+import { mount } from 'enzyme';
+import * as renderer from 'react-test-renderer';
+import ProductSinglePageComp from './productSinglePageComp'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { IProductModel } from 'src/models';
 
-describe('Product Single Page Comp', () => {
-  describe('when isLoading is false', () => {
-    it('should render children', () => {      
-      // const wrapper = mount(
-      //   <div>abc</div>
-      // );
-      // expect(wrapper.html()).toEqual('<div class=\"loadingScreen\">LoadingScreen</div>');
-      // wrapper.unmount();
-    });
+describe('Burger Menu', () => {
+  describe('Component should render right', () => {
+    let app:any;
+    const testMatch:any = {
+      path: '',
+      params: {},
+      url: ''
+    };
+    const article:IProductModel = {
+        id: 1,
+        type: "ability",
+        body: "body",
+        createdAt: new Date(1545167325 *1000),
+        image: "images/placeholder/abc.jpg",
+        name: "woda",
+        slug: "woda",
+        updatedAt: new Date(1545167325 *1000),
+        productType: "type"
+    }         
+    beforeEach(() => {
+      app = mount( 
+        <ProductSinglePageComp product={article} match={testMatch}/>
+      )
+    })
+    it('render correctly snapshot component', () => {
+      const tree = renderer
+      .create(<ProductSinglePageComp product={article} match={testMatch}/>)
+      .toJSON();
+      expect(tree).toMatchSnapshot();
+    })    
+    it.skip('should contain nav containar', () => {      
+      expect(app.find('ul').length).toEqual(1)        
+    })
+    it.skip('should contain elements', () => {
+      // 
+    })    
   });
-});
+}); 
